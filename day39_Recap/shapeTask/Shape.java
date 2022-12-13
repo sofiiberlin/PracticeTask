0a1367dc81c508d3268129a935b264c86f26b949
sofiiberlin <112969387+sofiiberlin@users.noreply.github.com>
2022-12-13 17:14:57 +0100

    added to String

---
+++ day39_Recap/shapeTask/Shape.java
@@ -0,0 +1,43 @@
+package day39_Recap.shapeTask;
+
+public class Shape {
+
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public int area(){
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    
+/*
+	Shape:
+	variables:
+			name
+	Encapsulate the field
+	Add a constructor to set the filed
+	Methods:
+		area(){}
+		perimeter(){}
+
+     */
+}
